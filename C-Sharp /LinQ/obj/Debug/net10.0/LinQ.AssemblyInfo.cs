bee1be168eeddb7f20942d3aed822ce53be32a32
Vaibhav Gupta <vg9400313@gmail.com>
2026-07-14 10:24:37 +0530

    Merge branch, resolved conflicts by removing bin, obj, and .DS_Store files, added gitignore

--- C-Sharp /LinQ/obj/Debug/net10.0/LinQ.AssemblyInfo.cs
+++ C-Sharp /LinQ/obj/Debug/net10.0/LinQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be824a0e14f271ec70086dc7be201800c65bbf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31bf7f1daf59c8f18bb448b0c71ca9dbe489ae8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- C-Sharp /UnderstandingEFCoreTransactionsSP/obj/Debug/net8.0/UnderstandingEFCoreTransactionsSP.AssemblyInfo.cs
+++ C-Sharp /UnderstandingEFCoreTransactionsSP/obj/Debug/net8.0/UnderstandingEFCoreTransactionsSP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnderstandingEFCoreTransactionsSP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be824a0e14f271ec70086dc7be201800c65bbf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31bf7f1daf59c8f18bb448b0c71ca9dbe489ae8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnderstandingEFCoreTransactionsSP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnderstandingEFCoreTransactionsSP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
